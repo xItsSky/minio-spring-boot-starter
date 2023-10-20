731f156e3635c2b30fa0892bfc2bc884ca9d4b77
xItsSky <emo.quentin@gmail.com>
2023-10-20 12:06:43 +0200

    feat(minio): init minio library
        reword 736a707 feat(minio): init minio library
        fixup 497048c feat(minio-service): Implement Minio Service
        fixup cd2af3b feat(minio): init minio library

---
+++ src/main/java/io/github/xitssky/minio/configuration/MinioConfigurationProperties.java
@@ -0,0 +1,78 @@
+package io.github.xitssky.minio.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+import java.util.List;
+
+/**
+ * Minio configuration properties class
+ *
+ * @author quentin
+ */
+@Getter
+@Setter
+@ConfigurationProperties("spring.minio")
+public class MinioConfigurationProperties {
+    /**
+     * The Minio url
+     */
+    private String url;
+
+    /**
+     * Is Proxy enabled
+     */
+    private boolean enableProxy = false;
+
+    /**
+     * The proxy type (to provide only if the proxy is enabled)
+     */
+    private String proxyType = "HTTP";
+
+    /**
+     * The proxy host (to provide only if the proxy is enabled)
+     */
+    private String proxyHost;
+
+    /**
+     * The proxy port (to provide only if the proxy is enabled)
+     */
+    private String proxyPort;
+
+    /**
+     * The Minio access key
+     */
+    private String accessKey;
+
+    /**
+     * The Minio secret key
+     */
+    private String secretKey;
+
+    /**
+     * The connection timeout
+     */
+    private Duration connectTimeout = Duration.ofSeconds(10);
+
+    /**
+     * The writing timeout
+     */
+    private Duration writeTimeout = Duration.ofSeconds(60);
+
+    /**
+     * The reading timeout
+     */
+    private Duration readTimeout = Duration.ofSeconds(10);
+
+    /**
+     * Are buckets auto created or not
+     */
+    private boolean autoCreateBucket = true;
+
+    /**
+     * All the buckets
+     */
+    private List<String> buckets;
+}
---
+++ src/main/java/io/github/xitssky/minio/exception/MinioRequestException.java
@@ -0,0 +1,31 @@
+package io.github.xitssky.minio.exception;
+
+import java.text.MessageFormat;
+
+/**
+ * The Exception thrown when a Minio request fail
+ *
+ * @author quentin
+ */
+public class MinioRequestException extends RuntimeException {
+    private static final String MESSAGE = "Fail to execute {0} request on Minio server";
+
+    /**
+     * Constructor
+     *
+     * @param request: the Minio request that fail
+     * @param cause:   the cause of the failure
+     */
+    public MinioRequestException(String request, Throwable cause) {
+        super(MessageFormat.format(MESSAGE, request), cause);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param request: the Minio request that fail
+     */
+    public MinioRequestException(String request) {
+        super(MessageFormat.format(MESSAGE, request));
+    }
+}
